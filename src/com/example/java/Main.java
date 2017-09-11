695dc831dda8d51cf5ca2765fc0bf22916b75d5b
Jose Juarez <jjuarez5631@students.mchenry.edu>
2017-09-10 22:33:30 -0500

    Modified the program to use the Switch structure correctly.

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -8,9 +8,9 @@ public class Main {
     public static void main(String[] args) {
 	//Display the user with options, validate choice and display choice
 
-        int userChoice;
+        int userChoice; //variable for the users input
         Scanner in = new Scanner(System.in);
-
+                        //while loop to validate users input and display information
         do {
             System.out.println("Welcome! Please make a selection from the following: ");
             System.out.println("1. Regular Burger.");
@@ -21,36 +21,35 @@ public class Main {
 
             userChoice = in.nextInt();
 
-            if(userChoice < 1 || userChoice > 5) {
-                System.out.println("");
-                System.out.println("INVALID SELECTION!");
-                System.out.println("");
+            System.out.println("You chose: ");
+
+            switch(userChoice){
+                case 1:
+                    System.out.println("A Regular Burger.");
+                    break;
+                case 2:
+                    System.out.println("A Regular Cheeseburger.");
+                    break;
+                case 3:
+                    System.out.println("A Bacon Cheeseburger.");
+                    break;
+                case 4:
+                    System.out.println("The 'Signature' Bacon Cheeseburger.");
+                    break;
+                case 5:
+                    System.out.println("A Jr Cheeseburger.");
+                    break;
+                default:
+                    System.out.println("An Invalid Selection.");
+                    System.out.println("");
+
             }
         }while (userChoice < 1 || userChoice > 5);
 
-        System.out.println("Thanks!");
         System.out.println("");
+        System.out.println("Thanks!");
 
-        System.out.println("You chose: ");
 
-        switch(userChoice){
-            case 1:
-                System.out.println("Regular Burger.");
-                break;
-            case 2:
-                System.out.println("Regular Cheeseburger.");
-                break;
-            case 3:
-                System.out.println("Bacon Cheeseburger.");
-                break;
-            case 4:
-                System.out.println("'Signature' Bacon Cheeseburger.");
-                break;
-            case 5:
-                System.out.println("Jr Cheeseburger.");
-                break;
-
-        }
 
     }
 }
